8d47e009ec592e8c8c4e6b2824b2938658fa57b8
周辉 <hui.zhou@lanmaoly.com>
2019-02-27 16:31:50 +0800

    hystrix+turbine dashboard监控

--- ribbon-client/src/main/java/com/zcloud/client/ribbon/ServiceRibbonApplication.java
+++ ribbon-client/src/main/java/com/zcloud/client/ribbon/ServiceRibbonApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +17,7 @@ import org.springframework.web.client.RestTemplate;
 @EnableDiscoveryClient
 @RestController
 @EnableCircuitBreaker
+@EnableHystrixDashboard
 public class ServiceRibbonApplication {
 
     public static void main(String[] args) {
