e8452205af5a5a093be4b6251e2a067d9f33f71e
周辉 <hui.zhou@lanmaoly.com>
2019-02-27 15:22:05 +0800

    使用ribbon实现客户端负载均衡robbin-server为客户端->访问client的hello服务

---
+++ ribbon-client/src/main/java/com/zcloud/client/ribbon/ServiceRibbonApplication.java
@@ -0,0 +1,36 @@
+package com.zcloud.client.ribbon;
+
+import com.zcloud.client.ribbon.service.HelloService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@RestController
+public class ServiceRibbonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceRibbonApplication.class, args);
+    }
+
+    @Bean
+    @LoadBalanced
+    RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    @Autowired
+    private HelloService helloService;
+
+    @RequestMapping("/")
+    public String Hello() {
+        return helloService.getHelloContent();
+    }
+}
