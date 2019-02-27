c17afec70139c34663b4f17d8397e2532f113799
周辉 <hui.zhou@lanmaoly.com>
2019-02-27 10:23:52 +0800

    spring clound eureka config server http://localhost:8888/config-client.properties http://localhost:8888/xyz/abc

--- config-server/src/main/java/com/zcloud/config/server/ConfigServerApplication.java
+++ config-server/src/main/java/com/zcloud/config/server/ConfigServerApplication.java
@@ -9,6 +9,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableConfigServer
 @SpringBootApplication
 public class ConfigServerApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(ConfigServerApplication.class, args);
     }
