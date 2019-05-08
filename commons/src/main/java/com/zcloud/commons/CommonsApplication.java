8f5ed69e80b914e6f070141cd8a867c472f67294
周辉 <hui.zhou@lanmaoly.com>
2019-05-08 11:05:48 +0800

    netfix-service discovery

--- commons/src/main/java/com/zcloud/commons/CommonsApplication.java
+++ commons/src/main/java/com/zcloud/commons/CommonsApplication.java
@@ -2,7 +2,14 @@ package com.zcloud.commons;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+/**
+ * eureka客户端
+ * 需要配置eureka的注册中心地址以及client应用的名称
+ * 如果应用名称一样则被视为集群的节点
+ */
+@EnableEurekaClient
 @SpringBootApplication
 public class CommonsApplication {
 
