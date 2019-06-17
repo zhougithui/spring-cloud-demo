a7d86babccf455c4dfca36f6e7b694ff66745042
周辉 <hui.zhou@lanmaoly.com>
2019-06-17 18:13:19 +0800

    ms service

---
+++ microservice-provider/src/main/resources/db/data.sql
@@ -0,0 +1,3 @@
+insert into T_user(id,username, name, age, balance) values(1, 'account1','张三', 20, 100.00);
+insert into T_user(id,username, name, age, balance) values(2, 'account2','李四', 23, 180.00);
+insert into T_user(id,username, name, age, balance) values(3, 'account3','王五', 30, 320.00);
