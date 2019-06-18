f831df0e360e3ff6b2c4fc61ff50365208cf66bc
周辉 <hui.zhou@lanmaoly.com>
2019-06-18 09:15:53 +0800

    ms consumer

--- microservice-consumer/src/main/java/com/awake/cloud/entity/UserEntity.java
+++ microservice-consumer/src/main/java/com/awake/cloud/entity/UserEntity.java
@@ -1,32 +1,21 @@
 package com.awake.cloud.entity;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
-import javax.persistence.*;
 import java.math.BigDecimal;
 
 /**
  * 忽略懒加载带来的json序列化问题
  * No serializer found for class org.hibernate.proxy.pojo.javassist.JavassistLazyInitializer
  */
-@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
-@Entity(name = "T_USER")
 public class UserEntity {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-    @Column
     private String username;
 
-    @Column
     private String name;
 
-    @Column
     private Integer age;
 
-    @Column
     private BigDecimal balance;
 
     public Long getId() {
