9afd2f187ab2caa9d1c4ea9f5e0f3973fda7812e
周辉 <hui.zhou@lanmaoly.com>
2019-06-19 10:36:34 +0800

    hystrix dashboard

--- microservice-consumer-feign-hystrix/src/main/java/com/awake/cloud/FeignHystrixConsumerApplication.java
+++ microservice-consumer-feign-hystrix/src/main/java/com/awake/cloud/FeignHystrixConsumerApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
@@ -11,6 +12,7 @@ import org.springframework.web.client.RestTemplate;
 @EnableCircuitBreaker
 @EnableDiscoveryClient
 @EnableFeignClients
+@EnableHystrixDashboard
 @SpringBootApplication
 public class FeignHystrixConsumerApplication {
 
