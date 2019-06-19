fd33ea998c48d5bb89f885f8a43fc428d98e1217
周辉 <hui.zhou@lanmaoly.com>
2019-06-19 11:52:16 +0800

    hystrix dashboard + turbine整合

--- microservice-consumer-ribbon-hystrix/src/main/java/com/awake/cloud/RibbonHystrixConsumerApplication.java
+++ microservice-consumer-ribbon-hystrix/src/main/java/com/awake/cloud/RibbonHystrixConsumerApplication.java
@@ -5,11 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @EnableCircuitBreaker
 @EnableEurekaClient
+@EnableHystrixDashboard
 @SpringBootApplication
 public class RibbonHystrixConsumerApplication {
 
---
+++ microservice-hystrix-dashboard/src/main/java/com/awake/cloud/HystrixApplication.java
@@ -0,0 +1,16 @@
+package com.awake.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+@EnableEurekaClient
+@EnableHystrixDashboard
+@SpringBootApplication
+public class HystrixApplication {
+
+    public static void main(String[] args){
+        SpringApplication.run(HystrixApplication.class, args);
+    }
+}
